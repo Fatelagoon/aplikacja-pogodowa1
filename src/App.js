ce0bf37d17854b547b5c0386ab871bb59378425c
Kamil Korzeniewski <39214733+Fatelagoon@users.noreply.github.com>
2020-08-24 13:05:42 +0200

    App Forms

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Weather from "./components/Weather"
 const  API_KEY = "ad81f6523681e81bafcce379330a02d9"
 
 class App extends React.Component { // poniższe wyświetla zawartość Titles.js, oraz Form.js
-  getWeather = async() => {
+  getWeather = async(e) => {
+    e.preventDefault();
     const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=&{API_KEY}&units=metric');
 
     const data = await api_call.json(); // api_call konwertowane na format .json
@@ -17,7 +18,7 @@ class App extends React.Component { // poniższe wyświetla zawartość Titles.j
     return (
       <div>
         <Titles /> 
-        <Form />
+        <Form getWeather={this.getWeather}/>
         <Weather />
       </div>
     )
