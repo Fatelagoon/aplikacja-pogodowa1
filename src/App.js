dbcbd8faa2847bca1d56308493b0d566c80598fc
Kamil Korzeniewski <fatelagoon@gmail.com>
2020-09-19 15:01:58 +0200

    App, Form, Titles Weather
    "this" delated, class to const

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ class App extends React.Component { // poniższe wyświetla zawartość Titles.j
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
 
     const data = await api_call.json(); // api_call konwertowane na format .json
+
+    if (city && country) {
     console.log(data);
 
     this.setState({ //ściezki do danych odczytane z konsoli przeglądarki
@@ -35,6 +37,16 @@ class App extends React.Component { // poniższe wyświetla zawartość Titles.j
       description: data.weather[0].description,
       error: ""
     });
+} else {
+  this.setState({
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: "Please enter the value."
+  });
+  
 }
   render() {
     return (
