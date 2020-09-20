2ad148015dac7584c3d5ad0b44a4eea458e74a0a
Kamil Korzeniewski <fatelagoon@gmail.com>
2020-09-20 18:42:19 +0200

    Titler, Weather, background.jpg

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,32 @@
 import React from "react";
 
 const Weather = props => (
-    <div>
-        {props.city && props.country && <p> Location: {props.city}, {props.country}</p>} 
-        {props.temperature && <p>Temperature: {props.temperature}</p>}
-        {props.humidity && <p>Humidity: {props.humidity}</p>}
-        {props.description && <p>Conditions: {props.description}</p>}
-        {props.error && <p>{ props.error}</p>} 
+    <div className="Weather__info"> 
+        {
+        props.city && props.country && <p className="wether__key"> Location:
+            <span className="weather__value">{props.city}, {props.country}</span>
+        </p>
+        } 
+        {
+        props.temperature && <p className="wether__key">Temperature: 
+            <span className="weather__value">{props.temperature}</span>
+            </p>
+            }
+        {
+        props.humidity && <p className="wether__key">Humidity: 
+            <span className="weather__value">{props.humidity}</span>
+        </p>
+        }
+        {
+        props.description && <p className="wether__key">Conditions: 
+            <span className="weather__value">{props.description}</span>
+            </p>
+            }
+        {
+        props.error && <p className="wether__error">
+            <span>{ props.error}</span>
+            </p>
+            } 
     </div> 
 );
         
